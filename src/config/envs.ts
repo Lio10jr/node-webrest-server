7e7cb263771e55e0cd63c82777aca8e30d3e3c16
Lio10jr <kevinzambrano593@gmail.com>
2024-09-18 16:28:30 -0500

    Deleted variable PROD

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,6 +3,5 @@ import env from 'env-var';
 
 export const envs = {
     PORT: env.get('PORT').required().asPortNumber(),
-    PROD: env.get('PROD').required().asBool(),
     PATH_PUBLIC: env.get('PATH_PUBLIC').default('public').asString(),
 }
