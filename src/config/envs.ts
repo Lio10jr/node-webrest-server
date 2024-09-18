5e2df934300345aea6ff41a8c4ac951b1c34f0a9
Lio10jr <kevinzambrano593@gmail.com>
2024-09-18 16:19:27 -0500

    Update envs file

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,5 +4,5 @@ import env from 'env-var';
 export const envs = {
     PORT: env.get('PORT').required().asPortNumber(),
     PROD: env.get('PROD').required().asString(),
-    PATH_PUBLIC: env.get('PATH_PUBLIC').required().asString(),
+    PATH_PUBLIC: env.get('PATH_PUBLIC').default('public').asString(),
 }
